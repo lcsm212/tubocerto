d41b10ce2fe4541c12a9ce7fe397d09e5cd46c41
Luiz Claudio <74454646+lcsm212@users.noreply.github.com>
2021-11-11 18:25:27 -0300

    update

---
+++ src/main/java/com/tubocerto/tubocerto/Tubocerto2Application.java
@@ -0,0 +1,13 @@
+package com.tubocerto.tubocerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tubocerto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tubocerto2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tubocerto/tubocerto/Tubocerto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubocerto.tubocerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tubocerto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
